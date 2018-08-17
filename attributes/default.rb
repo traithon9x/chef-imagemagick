79237592b6af31d0d099b8e42459e87d47eacad5
Washington Botelho <wbotelhos@gmail.com>
2018-08-17 02:32:49 -0300

    up: bump version to 7.0.8-10

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-default['chef-imagemagick']['source']['checksum']  = '65d5ab117f559dea84f765d029ecd5a8ea384d95a7900260d981319ca523eff0'
+default['chef-imagemagick']['source']['checksum']  = '16068fd45bcde796d7d8cce299b48649718b6f4b6d583effec5737b9c922fca6'
 default['chef-imagemagick']['source']['extension'] = 'tar.gz'
 default['chef-imagemagick']['source']['uri']       = 'https://www.imagemagick.org/download'
-default['chef-imagemagick']['source']['version']   = '7.0.7-31'
+default['chef-imagemagick']['source']['version']   = '7.0.8-10'
 
 
 default['chef-imagemagick']['source']['folder_name'] = "ImageMagick-#{node['chef-imagemagick']['source']['version']}"
